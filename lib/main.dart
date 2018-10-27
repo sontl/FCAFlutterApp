f5328316e2474c334927c6da2c90bbd15bdd07e0
Son Tran Lam <sontl@code.edu.vn>
2018-10-27 19:00:22 +0800

    almost complete the course listing item

--- lib/main.dart
+++ lib/main.dart
@@ -57,25 +57,41 @@ class CourseInfoListItem extends StatefulWidget {
 class _CourseInfoListItemState extends State<CourseInfoListItem> {
   @override
   Widget build(BuildContext context) {
-    
+
+    Widget buildRatingStar(double rate, int noOfRate) {
+      var stars = <Widget>[];
+      for (var i=0; i < rate; i++) {
+        stars.add(Icon(Icons.star, color: Colors.orange,));
+      }
+      stars.add(Padding(padding: const EdgeInsets.fromLTRB(0.0, 0.0, 6.0, 0.0)));
+      stars.add(Text(rate.toString()));
+      stars.add(Padding(padding: const EdgeInsets.fromLTRB(0.0, 0.0, 6.0, 0.0)));
+      stars.add(Text( 
+        "(" + noOfRate.toString() + " ratings)", 
+        style: TextStyle(fontSize: 12.0),)
+      );
+      return Container(
+        width: 360.0,
+        child: Row(
+          children: stars,          
+        )
+      );
+    }
 
     return Card(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget> [
-          const ListTile(
+          ListTile(
             title: Text('The web developer bootcamp'),
             subtitle: Text('The only course you need to learn web development - HTML, CSS, JS, Node, and More!'),
-            leading: Icon(Icons.album)
+            leading: Image.network(
+              "https://udemy-images.udemy.com/course/480x270/1326292_4dcf.jpg", width: 90.0,),
           ),
           ButtonTheme.bar( // make buttons use the appropriate styles for cards
             child: ButtonBar(
               children: <Widget>[
-                Icon(Icons.star),
-                FlatButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () { /* ... */ },
-                ),
+                buildRatingStar(4.0, 13356),
               ],
             ),
           ),
